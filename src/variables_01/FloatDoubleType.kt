c632d43f3543d8e72fd09e5f62ea5f06526fe5d1
Ardelingga Pramesta Kusuma <ardelingga.pk@gmail.com>
2025-10-14 16:51:21 +0700

    Learn about Float and Double data type variable

---
+++ src/variables_01/FloatDoubleType.kt
@@ -0,0 +1,9 @@
+package variables_01
+
+fun main(){
+    val myNumber: Float = 2.5f
+    val mySecondNumber: Double = 2.0
+
+    println("My number is $myNumber")
+    println("My second number is $mySecondNumber")
+}
