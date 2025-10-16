7b3cd7089e6c8a4d5d66496a59934dfbed33939c
Ardelingga Pramesta Kusuma <ardelingga.pk@gmail.com>
2025-10-17 00:50:46 +0700

    Continue to learn multiple constructor in class kotlin

--- src/oop_07/Main.kt
+++ src/oop_07/Main.kt
@@ -1,21 +1,36 @@
 package oop_07
 
 fun main(){
-    val user = User("Ardelingga", "Pramesta Kusuma", 25)
-    val friend = User("John", "Smith", 30)
+    val user = User("Ardelingga")
+    val friend = User("John", "Smith" )
+
+    println("Name: ${user.name}")
+    println("LastName: ${user.lastName}")
+    println("Age: ${user.age}")
+
+    println("\n")
 
     println("Name: ${friend.name}")
+    println("LastName: ${friend.lastName}")
+    println("Age: ${friend.age}")
 }
 
-class User(name: String, var lastName: String, var age:Int){
-    var name: String
+class User(var name: String, var lastName: String, var age:Int){
+//    var name: String
+//
+//    init { 
+//        if (name.lowercase().startsWith("a")){
+//            this.name = name
+//        } else {
+//            this.name = "User"
+//            println("The name doesn't start with the letter 'a' or 'A'")
+//        }
+//    }
 
-    init {
-        if (name.lowercase().startsWith("a")){
-            this.name = name
-        } else {
-            this.name = "User"
-            println("The name doesn't start with the letter 'a' or 'A'")
-        }
+    constructor(name:String) : this(name, "Lastname", 0){
+        println("2nd")
+    }
+    constructor(name:String, lastName: String): this(name, lastName, 0){
+        println("3rd")
     }
 }
