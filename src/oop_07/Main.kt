7a4474e55e80d36bb67e19dad1605806dbad0029
Ardelingga Pramesta Kusuma <ardelingga.pk@gmail.com>
2025-10-17 13:51:14 +0700

    Learn default constructor in class

--- src/oop_07/Main.kt
+++ src/oop_07/Main.kt
@@ -13,6 +13,13 @@ fun main(){
     println("Name: ${friend.name}")
     println("LastName: ${friend.lastName}")
     println("Age: ${friend.age}")
+
+    val user2 = User2(age = 19, firstName = "Ioana", lastName = "Dobbi")
+    val user2Friend = User2("John", "Smith")
+}
+
+class User2(var firstName: String, lastName: String = "LastName", age: Int = 0){
+
 }
 
 class User(var name: String, var lastName: String, var age:Int){
